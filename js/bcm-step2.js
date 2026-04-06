7f64132e628a2de1ad3a57d1bd73d4cb02eceb41
armandhammer <46571303+armandhammer@users.noreply.github.com>
2026-04-06 15:10:22 -0600

    Update bcm-step2.js

--- js/bcm-step2.js
+++ js/bcm-step2.js
@@ -58,7 +58,7 @@ function createLeafletMap() {
 
 function createSatelliteBasemap() {
     return L.tileLayer(
-        "https://server.arcgisonline.com/ArcGIS/rest/services/USGSNAIPImageryIndex/MapServer/tile/{z}/{y}/{x}",tile/{z}/{y}/{x}",/tile/{z}/{y}/{x}",
+        "https://naip.arcgis.com/arcgis/rest/services/NAIP/ImageServer/tile/{z}/{y}/{x}",tile/{z}/{y}/{x}",/tile/{z}/{y}/{x}",
         { attribution: "Imagery (c) Esri", maxZoom: 19 }
     );
 }
