3465edd247a2bcc8c2625474617c12c4cec2a3e6
armandhammer <46571303+armandhammer@users.noreply.github.com>
2026-04-26 14:56:17 -0600

    Update et-charts.js

--- myJS_scripts/et-charts.js
+++ myJS_scripts/et-charts.js
@@ -91,8 +91,8 @@ function drawETChart(data) {
     // svg
     var svg = d3.select("#et-chart")
         .append("svg")
-        .attr("width", containerWidth)
-        .attr("height", height + margin.top + margin.bottom)
+        .attr("width", containerWidth - 4)
+        .attr("height", containerHeight - 4)
         .append("g")
         .attr("transform", `translate(${margin.left},${margin.top})`);
 
